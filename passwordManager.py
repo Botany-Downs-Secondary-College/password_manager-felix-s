6d0460622b61a32191182f737629579376404def
FelixSua <78829670+FelixSua@users.noreply.github.com>
2021-03-27 19:05:40 +1300

    v2, added login system
    account details are not stored separately for each different user yet, still figuring out
    anyone who can login can see all account details the program has store, including other users.

--- passwordManager.py
+++ passwordManager.py
@@ -2,35 +2,70 @@
 #creates and displays passwords
 
 #variables and lists
-accountsList = []
+accountsList = [["testa","aa","aaa"], ["testb","bb","bbb"]]
+masterAccountsList = [["test", "12345"], ["test2", "123456"]]
 
 #main
 print("Hi, this program will allow you to craate, store, and view usernames and passwords you have registered. \nYou must be 13 or older to use this program.")
-name = input("What is your name? ")
-age = int(input("How old are you? "))
-if age < 13:
-    print('Sorry {}. You do not meet the age requirement to use this program.'.format(name))
-    exit()
-else:
-    print('Hello {}'.format(name))
-    while True:
-        print("Choose a mode by entering a number")
-        print("1. Store new account details")
-        print("2. View Accounts")
-        print("3. Remove an Account")
-        print("4. exit")
-        options = input()
-        if options == '1':
-            addUsername = input('Please enter the username of this account: ')
-            addPassword = input('Please enter the password of this account: ')
-            accountsList.append([addUsername, addPassword])
-        elif options == '2':
-            print('Choose what account you would like to remove.')
-            print(accountsList)
-        elif options == '3':
-            print("This option is not implemented yet.")
-        elif options == '4':
-            print('bai bai')
+while True:
+    print("Choose a mode by entering a number: ")
+    print("1. Sign up")
+    print("2. Login")
+    print("3. exit")
+    optionsOne = input()
+    if optionsOne == "1":
+        age = int(input("How old are you? "))
+        if age < 13:
+            print('Sorry. You do not meet the age requirement to use this program.')
             exit()
         else:
-            print('Please pick from the options displayed.')
+            addMasterUsername = input("What is the username of this account? ")
+            addMasterPassword = input("What is the password of this account? ")
+            masterAccountsList.append([addMasterUsername, addMasterPassword])
+            print("Account Created!")
+    elif optionsOne == "2":
+        MasterUsername = input("Username: ")
+        MasterPassword = input("Password: ")
+        for info in masterAccountsList:
+            if MasterUsername == info[0] and MasterPassword == info[1]:
+                print('Welcome, {}'.format(MasterUsername))
+                while True:
+                    print("Choose a mode by entering a number: ")
+                    print("1. Store new account details")
+                    print("2. View Accounts")
+                    print("3. Remove an Account")
+                    print("4. exit")
+                    optionsTwo = input()
+                    if optionsTwo == '1':
+                        addUsername = input('Please enter the username of this account: ')
+                        addPassword = input('Please enter the password of this account: ')
+                        addPurpose = input('What are these account details for?')
+                        accountsList.append([addPurpose, addUsername, addPassword])
+                    elif optionsTwo == '2':
+                        print('Here are the account details you have stored: ')
+                        for i in accountsList:
+                            print("{}\nUsername: {}\nPassword: {}".format(i[0], i[1], i[2]))
+                    elif optionsTwo == '3':
+                        print("This option is still in development.")
+                        continue
+                        print("Name the account you would like to remove.")
+                        for i, val in enumerate(accountsList, start=1):
+                            print(i, ".", val[0])
+                        removeInput = input()
+                    elif optionsTwo == '4':
+                        print('bai bai')
+                        exit()
+                    else:
+                        print('Please pick from the options displayed.')
+            else:
+                continue
+        else:
+            print("Incorrect login details")
+
+    elif optionsOne =="3":
+        print("bai bai")
+        exit()
+    else:
+        print("Please pick from the options displayed")
+
+
